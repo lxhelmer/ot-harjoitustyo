66c6e9c399753c135e5b0f53e4a0dd56b0f86f55
lxhelmer <helmer.lauronen@gmail.com>
2022-05-24 23:15:24 +0300

    final push

--- src/tests/sql_test.py
+++ src/tests/sql_test.py
@@ -1,3 +1,14 @@
 import unittest
+from services.tables import *
+from services.connection import get_connection
+
+class TestTables(unittest.TestCase):
+    def setUp(self):
+        print("")
+
+    def test_connection_setup(self):
+        testcon = get_connection()
+        testcur = testcon.cursor()
+        self.assertEqual(type(testcur),type(setup()))
 
 
